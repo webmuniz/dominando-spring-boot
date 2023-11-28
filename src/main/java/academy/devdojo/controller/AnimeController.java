3733efbac5c50379d98a64f47178340345c1b539
emunizcd2 <elvis.muniz@cd2.com.br>
2023-11-28 16:57:50 -0300

    Exercice 1, GetMapping

---
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -0,0 +1,17 @@
+package academy.devdojo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = {"v1/animes", "v1/animes/"})
+public class AnimeController {
+
+    @GetMapping
+    public List<String> list() {
+        return List.of("Dragon Ball", "Hellsing", "Berserk");
+    }
+}
